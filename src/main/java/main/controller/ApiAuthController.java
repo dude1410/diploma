1f0970cd14ba092339c65c4c453103b9e7757242
dude <dude1410@mail.ru>
2021-03-26 22:58:35 +0300

    corrected auth check

--- src/main/java/main/controller/ApiAuthController.java
+++ src/main/java/main/controller/ApiAuthController.java
@@ -1,20 +1,21 @@
 package main.controller;
 
 import main.api.response.AuthResponse;
+import main.service.AuthService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiAuthController {
 
-	private final AuthResponse authResponse;
+	private final AuthService authService;
 
-	public ApiAuthController(AuthResponse authResponse) {
-		this.authResponse = authResponse;
+	public ApiAuthController(AuthService authService) {
+		this.authService = authService;
 	}
 
 	@RequestMapping("/api/auth/")
-	private AuthResponse authResponse(){
-		return authResponse;
+	private AuthResponse response(){
+		return authService.getAuthResponse();
 	}
 }
